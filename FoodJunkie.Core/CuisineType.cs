3daf065715188da87692d5a667e42a3e84805016
Carl Yao Agbenyega <carl.agbenyega@gmail.com>
2019-08-22 14:35:54 -0600

    Add project files.

---
+++ FoodJunkie.Core/CuisineType.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodJunkie.Core
+{
+    public partial class Restaurant
+    {
+        public enum CuisineType
+        {
+            None,
+            Mexican,
+            Chinese,
+            Italian,
+            Indian
+        }
+    }
+}
---
+++ FoodJunkie.Data/IRestaurantData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FoodJunkie.Core;
+
+namespace FoodJunkie.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> Restaurants;
+    }
+}
