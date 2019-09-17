fb138935cbf8caf8a042fd72fb4afef22b28adbe
Carl Yao Agbenyega <carl.agbenyega@gmail.com>
2019-09-16 23:16:18 -0600

    EF Core package installation and DBContext configuration 1

---
+++ FoodJunkie.Data/FoodJunkieDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FoodJunkie.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodJunkie.Data
+{
+    public class FoodJunkieDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
