7cb2c5fca743b5bdbf02e5ce1d1e25f48d7d9915
Carl <carl.agbenyega@gmail.com>
2021-07-19 23:36:11 -0400

    Update Detail.cshtml.cs

--- FoodJunkie/Pages/Restaurants/Detail.cshtml.cs
+++ FoodJunkie/Pages/Restaurants/Detail.cshtml.cs
@@ -9,29 +9,33 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FoodJunkie.Pages.Restaurants
 {
-    public class DetailModel : PageModel
-    {
-        private readonly IRestaurantData restaurantData;
+		public class DetailModel : PageModel
+		{
+				private readonly IRestaurantData restaurantData;
 
-        [TempData]
-        public string Message { get; set; }
+				[TempData]
+				public string Message { get; set; }
 
-        public DetailModel(IRestaurantData restaurantData)
-        {
-            this.restaurantData = restaurantData;
-        }
+				public DetailModel(IRestaurantData restaurantData)
+				{
+						this.restaurantData = restaurantData;
+				}
 
-        public Restaurant Restaurant { get; set; }
-        
+				public Restaurant Restaurant { get; set; }
 
-        public IActionResult OnGet(int restaurantId)
-        {
-            Restaurant = restaurantData.GetById(restaurantId);
-            if (Restaurant == null)
-            {
-                return RedirectToPage("./NotFound");
-            }
-            return Page();
-        }
-    }
+				/// <summary>
+				/// Gets restaurant by Id
+				/// </summary>
+				/// <param name="restaurantId"></param>
+				/// <returns>Restaurant Object</returns>
+				public IActionResult OnGet(int restaurantId)
+				{
+						Restaurant = restaurantData.GetById(restaurantId);
+						if (Restaurant == null)
+						{
+								return RedirectToPage("./NotFound");
+						}
+						return Page();
+				}
+		}
 }
